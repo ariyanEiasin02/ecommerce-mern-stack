12b7e5682155ddbd6407d7c16fc86bfe255582e6
Md Eiasin <ariyaneiasin02@gmail.com>
2026-01-24 12:00:45 +0600

    feat: add ProductCard component and implement it in NewArrivals; update styles for product cards

--- ecommerce-frontend/components/HomePart/NewArrivals.tsx
+++ ecommerce-frontend/components/HomePart/NewArrivals.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import SectionTop from '../common/SectionTop'
+import ProductCard from '../card/ProductCard'
 
 const NewArrivals = () => {
   return (
     <section>
         <div className="container">
             <SectionTop />
-        </div>
+            <div className="row g-4">
+                {
+                    [1,2,3,4,5,6,7,8].map((item)=>(
+                        <div className="col-lg-3" key={item}>
+                            <ProductCard />
+                        </div>
+                    ))
+                }
+            </div>
+            </div>
     </section>
   )
 }
